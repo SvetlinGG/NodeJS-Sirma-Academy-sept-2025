a70d1a33f914b9d3f3fbd328e64f49cefb6bda28
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-09-28 22:54:17 +0200

    return

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 5000;
 app.get((req, res) => {
     res.send(`<h1>Hello my Web Server</h1>`);
 
-    res.end()
+    res.end();
 });
 
 app.listen(port, () => console.log('Server is listening on http//:localhost5000'));
