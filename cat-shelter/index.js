b874a7a6377d77b1113c01f69f96e352afb2f24c
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-09-30 23:15:35 +0200

    separated style css

--- cat-shelter/index.js
+++ cat-shelter/index.js
@@ -8,18 +8,20 @@ const addCat = require('./views/addCat.html');
 const port = 4000;
 
 const server = http.createServer((req, res)=> {
-    
+
+    if ( req.url === '/styles/site.css'){
+        res.writeHead(200, {
+            'content-type': 'text/css',
+        });
+        res.write(siteCss);
+        return res.end();
+    }   
 
     if ( req.url === '/'){
         res.writeHead(200, {
             'content-type': 'text/html',
         });
         res.write(indexHtml);
-    }else if ( req.url === '/styles/site.css'){
-        res.writeHead(200, {
-            'content-type': 'text/css',
-        });
-        res.write(siteCss);
     }else if(req.url === '/cats/add-breed'){
         res.write(addBreed);
 
