3fb6841df217c038c300ea095266aa660ddbbd7b
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-09-30 23:32:41 +0200

    catData template

---
+++ cat-shelter/views/cat.html.js
@@ -0,0 +1,12 @@
+module.exports = (catData) => `
+    <li>
+                    <img src="${catData.imageUrl}" alt="Black Cat">
+                    <h3></h3>
+                    <p><span>Breed: </span>Bombay Cat</p>
+                    <p><span>Description: </span>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</p>
+                    <ul class="buttons">
+                        <li class="btn edit"><a href="">Change Info</a></li>
+                        <li class="btn delete"><a href="">New Home</a></li>
+                    </ul>
+                </li>
+`;
