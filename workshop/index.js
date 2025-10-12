32787938c19ba049de89bb2af777f0e05272ff0a
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-12 20:21:35 +0200

    middleware

--- workshop/index.js
+++ workshop/index.js
@@ -5,5 +5,12 @@ const port = 4000;
 app.get('/', (req, res) => {
     res.send('<h1>Hello my first web server</h1>');
 });
+app.get('/about', (req, res, next) => {  // here is Middleware
+
+    console.log('Middleware');
+    next();
+}, (req, res) => {
+    res.send('This is about page')
+});
 
 app.listen(port, () => { console.log(`Server is listen on ${port}`)})
