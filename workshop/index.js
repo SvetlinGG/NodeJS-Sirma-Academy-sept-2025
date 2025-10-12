0596b101869d58983e40f12200fe4fdf066d0cd2
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-12 20:06:39 +0200

    setup

--- workshop/index.js
+++ workshop/index.js
@@ -6,5 +6,4 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello my first web server</h1>');
 });
 
-app.listen(port, () => { console.log(`Server is listen on ${port}`)
-})
+app.listen(port, () => { console.log(`Server is listen on ${port}`)})
